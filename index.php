ab95f394c4c06f67f78c4de0b0e71826c60fa174
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-12-21 23:16:36 -0300

    tratando exceptions (PDOException)

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
     $dsn = 'mysql:host=localhost;dbname=php_pdo';
     $usuario = 'root';
     $senha = '';
-    $conexao = new PDO($dsn, $usuario, $senha);
+    
 
+    try{
+        $conexao = new PDO($dsn, $usuario, $senha);
+    }catch(PDOException $e) {
+        echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
+        //registrar erro
+    }
+
+    
 ?>
