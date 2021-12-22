bc6cc2fc1d801aca89bc3587c4ba68eca59a2cda
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-12-22 00:11:16 -0300

    executando instruções SQL (exec)

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
 
     try{
         $conexao = new PDO($dsn, $usuario, $senha);
+
+        $query = '
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )   
+        ';
+
+        $retorno = $conexao->exec($query);
+
+        $query = '
+            delete from tb_usuarios
+        ';
+
+        $retorno = $conexao->exec($query);
+
+        echo $retorno;
     }catch(PDOException $e) {
         echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
         //registrar erro
