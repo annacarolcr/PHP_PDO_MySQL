cbb48d2f712d406dde110077dddc8ac25f091d36
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-12-23 23:47:05 -0300

    listando registros com foreach

--- index.php
+++ index.php
@@ -9,18 +9,31 @@
         $conexao = new PDO($dsn, $usuario, $senha);
 
         $query = '
-            select * from tb_usuarios where id = 6
+            select * from tb_usuarios
         ';
 
         $stmt = $conexao->query($query); //retorna um PDOStatement
 
-        $usuario = $stmt->fetch(PDO::FETCH_OBJ); 
-                                                    
-        echo'<pre>';                                
-        print_r($usuario);                            
-        echo'</pre>';
+        foreach($conexao->query($query) as $key => $value){
+            print_r($value);
+            echo '<hr>';
+        }
 
-        echo $usuario->nome;
+        //$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        /*                                            
+        echo'<pre>';                                
+        print_r($lista_usuarios);                            
+        echo'</pre>';
+        */
+
+        /*foreach ($lista_usuarios as $key => $value) {
+            print_r($value);
+            echo '<hr>';
+
+            echo $value['nome'];
+            echo '<hr>';
+        }*/
 
     }catch(PDOException $e) {
         echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
