cb603e1a293286a011442c22705bc0baa1b98308
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-12-23 21:59:44 -0300

    Fetch() ->  retornando apenas um registro

--- index.php
+++ index.php
@@ -9,19 +9,18 @@
         $conexao = new PDO($dsn, $usuario, $senha);
 
         $query = '
-            select * from tb_usuarios
+            select * from tb_usuarios where id = 6
         ';
 
         $stmt = $conexao->query($query); //retorna um PDOStatement
 
-        $lista = $stmt->fetchAll(PDO::FETCH_ASSOC); //PDO::FETCH_ASSOC -> utiliza índices associativos [nome] no array
-                                                    //PDO::FETCH_NUM -> utiliza índices numéricos [1] no array
-        echo'<pre>';                                //PDO::FETCH_BOTH -> utiliza índices numéricos e associativos [nome] [1] no array. Basta usar o Both ou deixar vazio
-        print_r($lista);                            //PDO::FETCH_OBJ -> retorna um array de objetos
+        $usuario = $stmt->fetch(PDO::FETCH_OBJ); 
+                                                    
+        echo'<pre>';                                
+        print_r($usuario);                            
         echo'</pre>';
 
-        echo $lista[0]['nome']; //para acessar os dados do array
-        //echo $lista[1]->nome; para acessar os dados do objeto retornado pelo PDO::FETCH_OBJ
+        echo $usuario->nome;
 
     }catch(PDOException $e) {
         echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
