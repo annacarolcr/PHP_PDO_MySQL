cac61c08fb6b678a4d5e18ee7eee1a482b03fa60
Anna Carolina Rocha <annacarolinarocha11@gmail.com>
2021-12-23 21:48:46 -0300

    fetchAll() -> trabalhando os tipos de retorno

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
 
         $stmt = $conexao->query($query); //retorna um PDOStatement
 
-        $lista = $stmt->fetchAll();
-
-        echo'<pre>';
-        print_r($lista);
+        $lista = $stmt->fetchAll(PDO::FETCH_ASSOC); //PDO::FETCH_ASSOC -> utiliza índices associativos [nome] no array
+                                                    //PDO::FETCH_NUM -> utiliza índices numéricos [1] no array
+        echo'<pre>';                                //PDO::FETCH_BOTH -> utiliza índices numéricos e associativos [nome] [1] no array. Basta usar o Both ou deixar vazio
+        print_r($lista);                            //PDO::FETCH_OBJ -> retorna um array de objetos
         echo'</pre>';
 
-        echo $lista[0][1];
-       
+        echo $lista[0]['nome']; //para acessar os dados do array
+        //echo $lista[1]->nome; para acessar os dados do objeto retornado pelo PDO::FETCH_OBJ
 
     }catch(PDOException $e) {
         echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
